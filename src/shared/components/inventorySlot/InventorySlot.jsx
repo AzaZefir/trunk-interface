3c1a81478d5cb20496d698231a377617956fafe1
azat <arykov.azat.kg@gmail.com>
2024-06-25 23:47:26 +0600

    create: inventorySlot shared component with custom hook and context

---
+++ src/shared/components/inventorySlot/InventorySlot.jsx
@@ -0,0 +1,34 @@
+import React, { useContext } from "react";
+
+import style from "./InventorySlot.module.scss";
+
+import { InventoryContext } from "../../../app/context/InventoryContext";
+import { useInventoryDragDrop } from "../../hooks/useInventoryDragDrop";
+
+const InventorySlot = ({ index, section, specialIndexes }) => {
+  const { inventoryData } = useContext(InventoryContext);
+  const item =
+    inventoryData && inventoryData[section] && inventoryData[section][index];
+
+  const { dragRef, dropRef, slotStyle } = useInventoryDragDrop(
+    index,
+    section,
+    item,
+    specialIndexes
+  );
+
+  return (
+    <div ref={dropRef} className={style.inventorySlot} style={slotStyle}>
+      {item && (
+        <img
+          ref={dragRef}
+          src={item.Data}
+          alt={item.Name}
+          className={style.itemImage}
+        />
+      )}
+    </div>
+  );
+};
+
+export default InventorySlot;
