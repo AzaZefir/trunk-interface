d127e145c1147975d72d833921fa9c5b01456d96
azat <arykov.azat.kg@gmail.com>
2024-06-29 00:28:33 +0600

    fix: calculation of items weight

--- src/utils/CalculateWeight.jsx
+++ src/utils/CalculateWeight.jsx
@@ -1,6 +1,10 @@
 export const calculateWeight = (data) => {
   return data.reduce(
-    (acc, item) => acc + (item && !isNaN(item.weight) ? item.weight : 0),
+    (acc, item) =>
+      acc +
+      (item && !isNaN(item.weight) && !isNaN(item.quantity)
+        ? item.weight * item.quantity
+        : 0),
     0
   );
 };
