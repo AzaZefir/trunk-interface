08ad7c099496f55f3e7a47916435be69ba3140bd
azat <arykov.azat.kg@gmail.com>
2024-06-30 19:23:54 +0600

    fix: exit btn style

--- src/shared/components/inventoryCloseBtn/InventoryCloseBtn.jsx
+++ src/shared/components/inventoryCloseBtn/InventoryCloseBtn.jsx
@@ -5,7 +5,7 @@ import style from "./InventoryCloseBtn.module.scss";
 const InventoryCloseBtn = ({ inventoryError }) => {
   return (
     <button className={style.inventoryclose}>
-      <span className={style.closeBadge}></span>ВЫЙТИ
+      <span className={style.closeBadge}>ESC</span>ВЫЙТИ
       <span className={style.errorText}>{inventoryError}</span>
     </button>
   );
